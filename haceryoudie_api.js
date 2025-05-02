146b76d51c91117daec8df59aa9a188dfb661809
NULLNOACCNO <soperhappy@gmail.com>
2025-05-02 05:09:12 +0300

    Create haceryoudie_api.js

---
+++ haceryoudie_api.js
@@ -0,0 +1,4 @@
+// haceryoudie_api.js - API Configuration
+const API_KEY = 'AIzaSyDWR4_-C5bjtqzguFITz70-jyTEYQKNfOg';
+const API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
+

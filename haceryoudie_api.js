76bae57ae2a131ed196337208ff220a3a0519ade
NULLNOACCNO <soperhappy@gmail.com>
2025-12-15 21:03:21 +0300

    Update haceryoudie_api.js

--- haceryoudie_api.js
+++ haceryoudie_api.js
@@ -1,4 +1,3 @@
-//
 // haceryoudie_api.js
 window.apiKey = 'AIzaSyA6WdwnzLNWg0DsScW9MHoPHgNtIVfbJmE';
 window.apiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
